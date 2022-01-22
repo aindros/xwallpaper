8804ba401f5052bd4c1bc7a62376c1c4290f2634
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2022-01-22 11:25:29 +0100

    Add config.def.h

---
+++ config.def.h
@@ -0,0 +1,27 @@
+/*
+    Copyright (C) 2022  Alessandro Iezzi aiezzi@alessandroiezzi.it
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+/*#define WITH_SECCOMP*/ /* Define this if you want seccomp support. */
+#define WITH_RANDR   /* Define this if you want RandR support.   */
+#define WITH_JPEG    /* Define this if you want JPEG support.    */
+#define WITH_PNG     /* Define this if you want PNG support.     */
+#define WITH_XPM     /* Define this if you want XPM support.     */
+
+#endif
